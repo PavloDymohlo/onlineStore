8989798cf24e483b913737051a2d53407ace847a
PavloDymohlo <pavelanatolevic777@gmail.com>
2025-03-24 16:32:03 +0200

    -add annotation

--- onlineStore/src/main/java/ua/dymohlo/onlineStore/util/GlobalConstants.java
+++ onlineStore/src/main/java/ua/dymohlo/onlineStore/util/GlobalConstants.java
@@ -1,5 +1,8 @@
 package ua.dymohlo.onlineStore.util;
 
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
 public final class GlobalConstants {
     public static final String STATUS_NEW = "NEW";
     public static final String STATUS_CANCELLED = "CANCELLED";
